15a954fd1d5f7366ca476fbc87b1d47d77c158b1
jsgumban <gumban.johnhel@gmail.com>
2025-08-13 11:58:50 +0800

    Update readme.md

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,6 +1,11 @@
 import { useState } from 'react';
 
+// Custom hook to manage local storage
+// It initializes the state with a value from localStorage or a default initial value
+// and provides a function to update both the state and localStorage.
 function useLocalStorage(key, initialValue) {
+	
+	
   const [value, setValue] = useState(() => {
     try {
       const saved = localStorage.getItem(key);
@@ -11,6 +16,7 @@ function useLocalStorage(key, initialValue) {
     }
   });
 
+	// Function to set the value in both state and localStorage
   const setStoredValue = (newValue) => {
     try {
       setValue(newValue);
