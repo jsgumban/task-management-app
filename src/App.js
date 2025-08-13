80183b14d53cd07348114d7c046d352cf5674a0a
jsgumban <gumban.johnhel@gmail.com>
2025-08-13 09:29:00 +0800

    Initial project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <div className="row justify-content-center">
+        <div className="col-lg-10">
+          <h1 className="text-center mb-4">Task Management Dashboard</h1>
+        </div>
+      </div>
     </div>
   );
 }
